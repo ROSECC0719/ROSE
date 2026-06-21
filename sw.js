95ab284329015a1aa612df2e0e0dae7998d111d5
ROSECC0719 <abby780719@gmail.com>
2026-06-22 01:39:11 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rose-v1-2';
+const CACHE_NAME = 'rose-v1-3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));

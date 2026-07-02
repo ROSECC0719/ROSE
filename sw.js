82e37d8035b0b4d56aac7fefefd093ffcf5a01bd
ROSECC0719 <abby780719@gmail.com>
2026-07-02 11:56:37 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rose-v4-2-offline-1';
+const CACHE_NAME = 'rose-v4-3-work';
 const ASSETS = ['./','./index.html','./manifest.json','./icon.svg','./sw.js'];
 
 self.addEventListener('install', event => {
